7f96f6ec24cf49ca295c03b448964722ea48ff8d
denis-papin <denis.pro@isd.lu>
2022-10-17 01:57:23 +0200

    3-device loop

--- src/main.rs
+++ src/main.rs
@@ -24,32 +24,85 @@ use mqtt::control::variable_header::ConnectReturnCode;
 use mqtt::packet::*;
 use mqtt::TopicFilter;
 use mqtt::{Decodable, Encodable, QualityOfService};
-use crate::publish::{connect_publisher, publish_2};
-
-
-fn main() {
-    // publish_2( "zigbee2mqtt/kitchen_lamp/set", 120, "ON" );
-    // publish();
-    subscribe();
-}
+use crate::publish::{connect_publisher, publish};
 
 fn generate_client_id() -> String {
     format!("/MQTT/rust/{}", Uuid::new_v4())
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone)]
-struct InterDim {
+// "color":{"hue":4,
+//          "saturation":97,
+//          "x":0.640625,
+//          "y":0.328125},
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+struct LampColor {
+    hue: u32,
+    saturation: u32,
+    x:f32,
+    y:f32,
+}
+
+
+// {"brightness":20,
+// "color":{"hue":4,
+//          "saturation":97,
+//          "x":0.640625,
+//          "y":0.328125},
+// "color_mode":"xy",
+// "color_temp":321,
+// "color_temp_startup":300,
+// "state":"OFF",
+// "update":{"state":"idle"}}'
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+struct LampRGB {
+    color : LampColor,
     brightness:u8,
-    linkquality:u8,
     state: String,
 }
 
 
-fn subscribe() {
-    // configure logging
-    env::set_var("RUST_LOG", env::var_os("RUST_LOG").unwrap_or_else(|| "info".into()));
-    env_logger::init();
+#[derive(Serialize, Deserialize, Debug, Clone, Eq, PartialEq)]
+struct LampBasic {
+    //color : LampColor,
+    brightness:u8,
+    state: String,
+}
 
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+struct InterDim {
+    brightness:u8,
+    // linkquality:u8,
+    state: String,
+}
+
+// {"consumption":0,
+// "current":0,
+// "device_temperature":32,
+// "energy":0,
+// "illuminance":0,
+// "illuminance_lux":0,
+// "linkquality":162,
+// "power":0,
+// "power_outage_count":1,
+// "state":"ON",
+// "voltage":240.2}
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+struct InterSwitch {
+    state: String,
+}
+
+
+#[derive(Debug, Clone)]
+struct Params {
+    pub server_addr : String,
+    pub client_id : String,
+    pub channel_filters: Vec<(TopicFilter, QualityOfService)>,
+    pub keep_alive :  u16,
+}
+
+
+fn parse_params() -> Params {
     let matches = App::new("sub-client")
         .author("Y. T. Chung <zonyitoo@gmail.com>")
         .arg(
@@ -97,43 +150,30 @@ fn subscribe() {
         .value_of("CLIENT_ID")
         .map(|x| x.to_owned())
         .unwrap_or_else(generate_client_id);
-    let channel_filters: Vec<(TopicFilter, QualityOfService)> = matches
-        .values_of("SUBSCRIBE")
-        .unwrap()
-        .map(|c| (TopicFilter::new(c.to_string()).unwrap(), QualityOfService::Level0))
-        .collect();
 
-    let keep_alive = 10;
+    // let channel_filters: Vec<(TopicFilter, QualityOfService)> = matches
+    //     .values_of("SUBSCRIBE")
+    //     .unwrap()
+    //     .map(|c| (TopicFilter::new(c.to_string()).unwrap(), QualityOfService::Level0))
+    //     .collect();
 
-    info!("Connecting to {:?} ... ", server_addr);
-    let mut stream = TcpStream::connect(server_addr).unwrap();
-    info!("Connected!");
 
-    info!("Client identifier {:?}", client_id);
-    let mut conn = ConnectPacket::new(client_id);
-    conn.set_clean_session(true);
-    conn.set_keep_alive(keep_alive);
-    let mut buf = Vec::new();
-    conn.encode(&mut buf).unwrap();
-    stream.write_all(&buf[..]).unwrap();
+    let channel_filters: Vec<(TopicFilter, QualityOfService)> = vec![
+        (TopicFilter::new("zigbee2mqtt/kitchen_inter_dim".to_string()).unwrap(), QualityOfService::Level0),
+        (TopicFilter::new("zigbee2mqtt/kitchen_lamp".to_string()).unwrap(), QualityOfService::Level0),
+        (TopicFilter::new("zigbee2mqtt/hall_inter_switch".to_string()).unwrap(), QualityOfService::Level0),
+    ];
 
-    let connack = ConnackPacket::decode(&mut stream).unwrap();
-    trace!("CONNACK {:?}", connack);
-
-    if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
-        panic!(
-            "Failed to connect to server, return code {:?}",
-            connack.connect_return_code()
-        );
+    Params {
+        server_addr : server_addr.to_string(),
+        client_id,
+        channel_filters,
+        keep_alive : 10,
     }
+}
 
-    // const CHANNEL_FILTER: &'static str = "typing-speed-test.aoeu.eu";
-    info!("Applying channel filters {:?} ...", channel_filters);
-    let sub = SubscribePacket::new(10, channel_filters);
-    let mut buf = Vec::new();
-    sub.encode(&mut buf).unwrap();
-    stream.write_all(&buf[..]).unwrap();
 
+fn wait_subpack(mut stream : &mut TcpStream) ->Result<(), String>   {
     loop {
         let packet = match VariablePacket::decode(&mut stream) {
             Ok(pk) => pk,
@@ -153,14 +193,18 @@ fn subscribe() {
             break;
         }
     }
+    Ok(())
+}
 
+fn ping_broker(stream : &mut TcpStream, params : &Params) -> Result<(), String> {
+    let params_clone = (*params).clone();
     let mut stream_clone = stream.try_clone().unwrap();
     thread::spawn(move || {
         let mut last_ping_time = Instant::now();
-        let mut next_ping_time = last_ping_time + Duration::from_secs((keep_alive as f32 * 0.9) as u64);
+        let mut next_ping_time = last_ping_time + Duration::from_secs((params_clone.keep_alive as f32 * 0.9) as u64);
         loop {
             let current_timestamp = Instant::now();
-            if keep_alive > 0 && current_timestamp >= next_ping_time {
+            if params_clone.keep_alive > 0 && current_timestamp >= next_ping_time {
                 info!("Sending PINGREQ to broker");
 
                 let pingreq_packet = PingreqPacket::new();
@@ -170,14 +214,40 @@ fn subscribe() {
                 stream_clone.write_all(&buf[..]).unwrap();
 
                 last_ping_time = current_timestamp;
-                next_ping_time = last_ping_time + Duration::from_secs((keep_alive as f32 * 0.9) as u64);
-                thread::sleep(Duration::new((keep_alive / 2) as u64, 0));
+                next_ping_time = last_ping_time + Duration::from_secs((params_clone.keep_alive as f32 * 0.9) as u64);
+                thread::sleep(Duration::new((params_clone.keep_alive / 2) as u64, 0));
             }
         }
     });
+    Ok(())
+}
 
+fn process_incomming_message(mut stream : &mut TcpStream, mut pub_stream: &mut TcpStream) -> Result<(), String> {
 
-    let (mut pub_stream, mut conn, connack) = connect_publisher();
+    let mut lamp_locks : u32 = 0;
+    let mut dim_locks : u32 = 0;
+    let mut switch_locks : u32 = 0;
+
+    let mut last_inter_switch = InterSwitch {
+        state: "".to_string(),
+    };
+
+    let mut last_inter_dim = InterDim {
+        brightness: 0,
+        // linkquality: 0,
+        state: "".to_string()
+    };
+
+    let mut last_lamp_rgb = LampRGB {
+        color: LampColor {
+            hue: 0,
+            saturation: 0,
+            x: 0.0,
+            y: 0.0
+        },
+        brightness: 0,
+        state: "".to_string()
+    };
 
     loop {
         let packet = match VariablePacket::decode(&mut stream) {
@@ -202,20 +272,155 @@ fn subscribe() {
                     }
                 };
                 info!("PUBLISH ({}): {}", publ.topic_name(), msg);
-                // {"brightness":92,"linkquality":192,"state":"ON"}
 
-                let r_info: Result<InterDim, _> = serde_json::from_str(msg);
-                let inter_dim = r_info.unwrap();
+                // dbg!(&publ);
+                
+                if (publ.topic_name()) == "zigbee2mqtt/kitchen_inter_dim" {
+                    let r_info: Result<InterDim, _> = serde_json::from_str(msg);
+                    let inter_dim = r_info.unwrap();
 
-                // TODO  REF_TAG : DUMMY_MESSAGES It seems that the dimmer sends message every minute or so,
-                //      It's not necessary to take action after each message that are identical.
-                //      Nevertheless, it helps keeping the pub_stream open....
-                //      The best way would be to be able to know if the pub_stream is open or not and re-open it if necessary.
-                info!("Here we are, {:?}", &inter_dim);
+                    if dim_locks > 0 {
+                        info!("⛔ DIMMER MESSAGE Here we are, {:?} ", &inter_dim);
+                        info!("DIMMER IS LOCKED BY THE DIMMER ({}): {}", publ.topic_name(), msg);
+                        dim_locks -= 1;
+                    } else {
 
-                publish_2(  &mut  pub_stream, /*&mut conn,*/ "zigbee2mqtt/kitchen_lamp/set", inter_dim.brightness, &inter_dim.state );
+                        if inter_dim == last_inter_dim {
+                            info!("⛔ DIMMER [same message], {:?} ", &inter_dim);
+                        } else {
+                            info!("🍺 DIMMER MESSAGE Here we are, {:?} ", &inter_dim);
+
+                            lamp_locks += 1;
+
+                            /*
+                            //let message = r#"{"brightness":100,"linkquality":189,"state":"ON"}"#.to_string();
+                            let message = format!("{{\"brightness\":{},\"state\":\"{}\"}}", inter_dim.brightness, &inter_dim.state);
+                             */
+                            let lamp_rgb = LampBasic {
+                                brightness: inter_dim.brightness,
+                                state: inter_dim.state.clone(),
+                            };
+
+                            let message = serde_json::to_string(&lamp_rgb).unwrap();
+                            publish(&mut pub_stream, "zigbee2mqtt/kitchen_lamp/set", &message);
+
+                            switch_locks += 1;
+                            let message = format!("{{\"state\":\"{}\"}}", &inter_dim.state);
+                            publish(&mut pub_stream, "zigbee2mqtt/hall_inter_switch/set", &message);
+                        }
+
+                    }
+                    last_inter_dim = inter_dim;
+                }
+
+
+                if (publ.topic_name()) == "zigbee2mqtt/kitchen_lamp" {
+
+                    let r_info: Result<LampRGB, _> = serde_json::from_str(msg);
+                    let lamp_rgb = r_info.unwrap();
+
+                    if lamp_locks > 0 {
+                        info!("⛔ LAMP Here we are, {:?} ", &lamp_rgb);
+                        info!("LAMP IS LOCKED BY THE DIMMER ({}): {}", publ.topic_name(), msg);
+                        lamp_locks -= 1;
+                    } else {
+
+                        if lamp_rgb == last_lamp_rgb {
+                            info!("⛔ LAMP [same message], {:?} ", &lamp_rgb);
+                        } else {
+                            info!("🍺 LAMP Here we are, {:?} ", &lamp_rgb);
+                            info!("PROCESS LAMP ({}): {}", publ.topic_name(), msg);
+                            dim_locks += 1;
+                            let message = format!("{{\"brightness\":{},\"state\":\"{}\"}}", lamp_rgb.brightness, &lamp_rgb.state);
+                            publish(&mut pub_stream, "zigbee2mqtt/kitchen_inter_dim/set", &message);
+
+                            switch_locks += 1;
+                            let message = format!("{{\"state\":\"{}\"}}", &lamp_rgb.state);
+                            publish(&mut pub_stream, "zigbee2mqtt/hall_inter_switch/set", &message);
+                        }
+                        last_lamp_rgb = lamp_rgb;
+                    }
+                }
+
+
+                if (publ.topic_name()) == "zigbee2mqtt/hall_inter_switch" {
+                    let r_info: Result<InterSwitch, _> = serde_json::from_str(msg);
+                    let inter_switch = r_info.unwrap();
+
+                    if switch_locks > 0 {
+                        info!("⛔ SWITCH Here we are, {:?} ", &inter_switch);
+                        info!("SWITCH IS LOCKED BY THE DIMMER ({}): {}", publ.topic_name(), msg);
+                        switch_locks -= 1;
+                    } else {
+
+                        if inter_switch == last_inter_switch {
+                            info!("⛔ SWITCH [same message], {:?} ", &inter_switch);
+                        } else {
+                            info!("🍺 SWITCH Here we are, {:?} ", &inter_switch);
+                            info!("PROCESS SWITCH ({}): {}", publ.topic_name(), msg);
+                            dim_locks += 1;
+                            let message = format!("{{\"state\":\"{}\"}}", &inter_switch.state);
+                            publish(&mut pub_stream, "zigbee2mqtt/kitchen_inter_dim/set", &message);
+
+                            lamp_locks += 1;
+                            let message = format!("{{\"state\":\"{}\"}}", &inter_switch.state);
+                            publish(&mut pub_stream, "zigbee2mqtt/kitchen_lamp/set", &message);
+                        }
+
+                    }
+                    last_inter_switch = inter_switch;
+                }
+
             }
             _ => {}
         }
     }
+    Ok(())
+}
+
+
+fn main() {
+    // publish_2( "zigbee2mqtt/kitchen_lamp/set", 120, "ON" );
+    // configure logging
+    env::set_var("RUST_LOG", env::var_os("RUST_LOG").unwrap_or_else(|| "info".into()));
+    env_logger::init();
+
+    let params = parse_params();
+
+    info!("Connecting to mqtt at {:?} ... ", &params.server_addr);
+    let mut stream = TcpStream::connect(&params.server_addr).unwrap();
+    info!("Connected!");
+
+    info!("Client identifier {:?}", &params.client_id);
+    let mut conn = ConnectPacket::new(&params.client_id);
+    conn.set_clean_session(true);
+    conn.set_keep_alive(params.keep_alive);
+    let mut buf = Vec::new();
+    conn.encode(&mut buf).unwrap();
+    stream.write_all(&buf[..]).unwrap();
+
+    let connack = ConnackPacket::decode(&mut stream).unwrap();
+    trace!("CONNACK {:?}", connack);
+
+    if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
+        panic!(
+            "Failed to connect to server, return code {:?}",
+            connack.connect_return_code()
+        );
+    }
+
+    info!("Applying channel filters {:?} ...", &params.channel_filters);
+    let sub = SubscribePacket::new(10, params.channel_filters.clone());
+    let mut buf = Vec::new();
+    sub.encode(&mut buf).unwrap();
+    stream.write_all(&buf[..]).unwrap();
+
+    let _ = wait_subpack(&mut stream);
+    let _ = ping_broker(&mut stream, &params);
+
+    // Open the stream to publish the response(s)
+    let mut pub_stream= connect_publisher();
+
+    // Process incomming message from the dimmer
+    let _ = process_incomming_message(&mut stream, &mut pub_stream);
 }
--- src/publish.rs
+++ src/publish.rs
@@ -2,9 +2,9 @@
 // extern crate log;
 
 use std::env;
-use std::io::{self, Write};
+use std::io::{Write};
 use std::net::TcpStream;
-use std::thread;
+
 
 use clap::{App, Arg};
 
@@ -19,7 +19,7 @@ fn generate_client_id() -> String {
     format!("/MQTT/rust/{}", Uuid::new_v4())
 }
 
-pub fn connect_publisher() -> ( TcpStream, ConnectPacket, ConnackPacket) {
+pub fn connect_publisher() -> /*( TcpStream, ConnectPacket, ConnackPacket)*/ TcpStream {
 
     info!("Connect the publisher");
 
@@ -50,10 +50,11 @@ pub fn connect_publisher() -> ( TcpStream, ConnectPacket, ConnackPacket) {
         );
     }
 
-    (stream, conn, connack)
+    // (stream, conn, connack)
+    stream
 }
 
-pub fn publish_2( mut stream : &mut TcpStream, /*, conn : &mut ConnectPacket,*/ topic: &str, brightness: u8, state: &str ) {
+pub fn publish(/*mut*/ stream : &mut TcpStream, topic: &str, message: &str ) {
 
     let channel_filters: Vec<(TopicFilter, QualityOfService)> = vec![(TopicFilter::new(topic).unwrap(), QualityOfService::Level0)];
 
@@ -65,10 +66,7 @@ pub fn publish_2( mut stream : &mut TcpStream, /*, conn : &mut ConnectPacket,*/ 
 
     let channels: Vec<TopicName> = vec![TopicName::new(topic).unwrap()];
 
-    //let message = r#"{"brightness":100,"linkquality":189,"state":"ON"}"#.to_string();
-    let message = format!("{{\"brightness\":{},\"state\":\"{}\"}}",  brightness, state);
-
-    println!("Message : {}", &message);
+    println!("Message : {}", message);
 
     for chan in &channels {
         // let publish_packet = PublishPacket::new(chan.clone(), QoSWithPacketIdentifier::Level0, message.clone());
@@ -83,155 +81,4 @@ pub fn publish_2( mut stream : &mut TcpStream, /*, conn : &mut ConnectPacket,*/ 
 
 }
 
-pub fn _old_publish() {
-    // configure logging
-    env::set_var("RUST_LOG", env::var_os("RUST_LOG").unwrap_or_else(|| "info".into()));
-    env_logger::init();
 
-    let matches = App::new("sub-client")
-        .author("Y. T. Chung <zonyitoo@gmail.com>")
-        .arg(
-            Arg::with_name("SERVER")
-                .short("S")
-                .long("server")
-                .takes_value(true)
-                .required(true)
-                .help("MQTT server address (host:port)"),
-        )
-        .arg(
-            Arg::with_name("SUBSCRIBE")
-                .short("s")
-                .long("subscribe")
-                .takes_value(true)
-                .multiple(true)
-                .required(true)
-                .help("Channel filter to subscribe"),
-        )
-        .arg(
-            Arg::with_name("USER_NAME")
-                .short("u")
-                .long("username")
-                .takes_value(true)
-                .help("Login user name"),
-        )
-        .arg(
-            Arg::with_name("PASSWORD")
-                .short("p")
-                .long("password")
-                .takes_value(true)
-                .help("Password"),
-        )
-        .arg(
-            Arg::with_name("CLIENT_ID")
-                .short("i")
-                .long("client-identifier")
-                .takes_value(true)
-                .help("Client identifier"),
-        )
-        .get_matches();
-
-    let server_addr = matches.value_of("SERVER").unwrap();
-    let client_id = matches
-        .value_of("CLIENT_ID")
-        .map(|x| x.to_owned())
-        .unwrap_or_else(generate_client_id);
-    let channel_filters: Vec<(TopicFilter, QualityOfService)> = matches
-        .values_of("SUBSCRIBE")
-        .unwrap()
-        .map(|c| (TopicFilter::new(c.to_string()).unwrap(), QualityOfService::Level0))
-        .collect();
-
-    let keep_alive = 10;
-
-    info!("Connecting to {:?} ... ", server_addr);
-    let mut stream = TcpStream::connect(server_addr).unwrap();
-    info!("Connected!");
-
-    info!("Client identifier {:?}", client_id);
-    let mut conn = ConnectPacket::new(client_id);
-    conn.set_clean_session(true);
-    conn.set_keep_alive(keep_alive);
-    let mut buf = Vec::new();
-    conn.encode(&mut buf).unwrap();
-    stream.write_all(&buf[..]).unwrap();
-
-    let connack = ConnackPacket::decode(&mut stream).unwrap();
-    trace!("CONNACK {:?}", connack);
-
-    if connack.connect_return_code() != ConnectReturnCode::ConnectionAccepted {
-        panic!(
-            "Failed to connect to server, return code {:?}",
-            connack.connect_return_code()
-        );
-    }
-
-    info!("Applying channel filters {:?} ...", channel_filters);
-    let sub = SubscribePacket::new(10, channel_filters);
-    let mut buf = Vec::new();
-    sub.encode(&mut buf).unwrap();
-    stream.write_all(&buf[..]).unwrap();
-
-    let channels: Vec<TopicName> = matches
-        .values_of("SUBSCRIBE")
-        .unwrap()
-        .map(|c| TopicName::new(c.to_string()).unwrap())
-        .collect();
-
-    let user_name = matches.value_of("USER_NAME").unwrap_or("<anonym>");
-
-    let mut cloned_stream = stream.try_clone().unwrap();
-    /*
-    thread::spawn(move || {
-        loop {
-            let packet = match VariablePacket::decode(&mut cloned_stream) {
-                Ok(pk) => pk,
-                Err(err) => {
-                    error!("Error in receiving packet {:?}", err);
-                    continue;
-                }
-            };
-            trace!("PACKET {:?}", packet);
-
-            match packet {
-                VariablePacket::PingreqPacket(..) => {
-                    let pingresp = PingrespPacket::new();
-                    info!("Sending Ping response {:?}", pingresp);
-                    pingresp.encode(&mut cloned_stream).unwrap();
-                }
-                VariablePacket::DisconnectPacket(..) => {
-                    break;
-                }
-                _ => {
-                    // Ignore other packets in pub client
-                }
-            }
-        }
-    });
-    */
-
-
-    // let stdin = io::stdin();
-    //loop {
-        // print!("{}: ", user_name);
-        // io::stdout().flush().unwrap();
-        //
-        // let mut line = String::new();
-        // stdin.read_line(&mut line).unwrap();
-        //
-        // if line.trim_end() == "" {
-        //     continue;
-        // }
-        //
-        // let message = format!("{}: {}", user_name, line.trim_end());
-
-        let message = r#"{"brightness":10,"linkquality":189,"state":"OFF"}"#.to_string();
-
-        for chan in &channels {
-            // let publish_packet = PublishPacket::new(chan.clone(), QoSWithPacketIdentifier::Level0, message.clone());
-            let publish_packet = PublishPacketRef::new(chan, QoSWithPacketIdentifier::Level0, message.as_bytes());
-            let mut buf = Vec::new();
-            publish_packet.encode(&mut buf).unwrap();
-            stream.write_all(&buf[..]).unwrap();
-        }
-    //}
-}
