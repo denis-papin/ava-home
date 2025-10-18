00049f5289780022db8b4d9c08da7746dd13c1a7
denis-papin <denis.1@crespe.fr>
2025-10-18 09:00:12 +0200

    New common config

--- event-storage/src/main.rs
+++ event-storage/src/main.rs
@@ -10,9 +10,28 @@ use crate::message_enum::MessageEnum;
 use ava_toolkit::hard_loop::HardLoop;
 use ava_toolkit::init_loop::process_initialization_message;
 use ava_toolkit::processing::process_incoming_message;
+use common_config::conf_reader::{read_config, read_env};
+use common_config::properties::{get_prop_value, set_prop_values};
 
 mod message_enum;
 
+
+fn read_props_or_die(property_name: &str) -> String {
+    let value = match get_prop_value(property_name) {
+        Ok(file) => file,
+        Err(e) => {
+            error!("{}", e);
+            panic!("Cannot find the property")
+        }
+    };
+    value
+}
+
+/// Accept parameters from the command line
+/// * --config-file [optional] : the path to the .ava-config.json file (or from the AVA_ENV environment variable)
+/// * --cluster-profile : the name of the cluster profile
+///
+/// By default, the program will look for the .doka-config.json file in the user's base folder
 #[tokio::main]
 async fn main() {
 
@@ -20,8 +39,24 @@ async fn main() {
     env_logger::init();
 
     info!("Starting AVA event-storage 0.5.0");
+    
+    const PROJECT_CODE: &str = "event-storage";
+    const VAR_NAME: &str = "AVA_ENV";
 
-    let mut domo_factory: DomoticFactory<MessageEnum> = DomoticFactory::new(r"/home/denis/Projects/wks-ava-home/ava-home/event-storage/resources/modules.json");
+    let o_config_file = read_env(&VAR_NAME);
+
+    // Read the application config's file
+    println!("😎 Config file using PROJECT_CODE={} VAR_NAME={}", PROJECT_CODE, VAR_NAME);
+
+    let props = read_config(PROJECT_CODE, &o_config_file, &Some("AVA_CLUSTER_PROFILE".to_string()));
+    set_prop_values(props);
+
+    let module_file = read_props_or_die("module");
+    let mqtt_port = read_props_or_die("mqtt.port").parse::<u16>().unwrap(); // TODO 
+    let mqtt_user = read_props_or_die("mqtt.user");
+    let mqtt_password = read_props_or_die("mqtt.password");
+    
+    let mut domo_factory: DomoticFactory<MessageEnum> = DomoticFactory::new(module_file);
     domo_factory.build_devices();
 
     let all_loops = domo_factory.build_loops();
@@ -31,10 +66,10 @@ async fn main() {
     let args: Vec<String> = vec![];
     let channels = DomoticFactory::extract_channel_from_devices(&device_to_listen);
 
-    let mut mqttoptions = MqttOptions::new(&channels.client_id, &channels.server_addr, 1883);
+    let mut mqttoptions = MqttOptions::new(&channels.client_id, &channels.server_addr, mqtt_port);
     mqttoptions.set_keep_alive(Duration::from_secs(channels.keep_alive as u64));
     mqttoptions.set_clean_start(true);
-    mqttoptions.set_credentials("ava", "avatece3.X");
+    mqttoptions.set_credentials(mqtt_user, mqtt_password);
 
     let (mut client, mut eventloop) = AsyncClient::new(mqttoptions, 10);
 
